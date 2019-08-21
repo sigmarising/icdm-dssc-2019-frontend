dabb2f1187d1830fd0ff5b33dd9ab96de6b3a023
Jason Zhang <sigmarising@hotmail.com>
2019-08-21 15:08:11 +0800

    MOD: the title of page

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: process.env.npm_package_name,
+    title: process.env.npm_package_name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -54,7 +54,7 @@ export default {
     proxy: true
   },
   proxy: {
-    '/api/': 'http://localhost:8000',
+    '/api/': 'http://localhost:8000'
   },
   /*
    ** vuetify module configuration
